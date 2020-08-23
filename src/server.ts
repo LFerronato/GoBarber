38d6edb29c76e6caea8a11a08a63e03cc8d3472b
Lucas Ferronato <lucas.fe.pelle@gmail.com>
2020-08-23 14:44:46 -0300

    static files route

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import 'reflect-metadata'
 import express from 'express'
 import routes from './routes'
 
+import uploadConfig from './configs/upload'
+
 import './database'
 
 const app = express()
 app.use(express.json())
-
+app.use('/files', express.static(uploadConfig.directory)) // tem que ser GET mesmo
 app.use(routes)
 
 app.listen(3333, () => {
