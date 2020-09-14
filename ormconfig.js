584871f5a521e2c003cf9e62a17602d8515fff0c
Lucas Ferronato <lucas.fe.pelle@gmail.com>
2020-09-13 23:51:50 -0300

    create env example

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+module.exports = {
+  "type": "postgres",
+  "host": process.env.POSTGRES_HOST,
+  "port": 5432,
+  "username": process.env.POSTGRES_USERNAME,
+  "password": process.env.POSTGRES_PASSWORD,
+  "database": process.env.POSTGRES_DATABASE,
+  "entities": [
+    "./src/models/*.ts"
+  ],
+  "migrations": [
+    "./src/database/migrations/*.ts"
+  ],
+  "cli": {
+    "migrationsDir": "./src/database/migrations"
+  }
+}
